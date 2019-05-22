8d3bc711c5a0b6bd8ba65c5b6b8d707c3b2120bd
SlenderFox <SlenderFox@outlook.com>
2019-05-22 16:37:10 +1000

    Started work on beats spawning at correct times

--- MuppetSymphony/Assets/Scripts/Beat.cs
+++ MuppetSymphony/Assets/Scripts/Beat.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
-public class Beat
+public class Beat : MonoBehaviour
 {
-    public ushort us_Lane;
-    public uint ui_Delay;
+    public float f_speed = 150.0f;
+ 
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, -f_speed));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
 }
---
+++ MuppetSymphony/Assets/Scripts/Beatdata.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Beatdata
+{
+    public ushort us_lane;
+    public uint ui_delay;
+}
