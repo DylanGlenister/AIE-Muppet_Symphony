6ab62361f3a32bab77cd3b9ae0768d8660e6e724
SlenderFox <SlenderFox@outlook.com>
2019-05-24 13:22:49 +1000

    Got a main menu scene

---
+++ MuppetSymphony/Assets/Scripts/MenuSpash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpash : MonoBehaviour
+{
+    public GameObject m_goSplashScreen;
+    public GameObject m_goMenuPanel;
+
+    private void Awake()
+    {
+        Invoke("HideSplash", 3.0f);
+    }
+
+    public void HideSplash()
+    {
+        m_goSplashScreen.SetActive(false);
+        m_goMenuPanel.SetActive(true);
+    }
+}
